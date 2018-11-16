89956fb89a05b690f845828f06f91fe367b2c5e1
hengruo <hengruoz@gmail.com>
2018-11-16 01:15:06 -0500

    TINY FIX

--- init.sh
+++ init.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-if [ -d ./public/ ]; then
-    rm -rf ./public/
-fi
 if [ -d ./node_modules/ ]; then
     rm -rf ./node_modules/
 fi
-if [ -d ./.deploy_git/ ]; then
-    rm -rf ./.deploy_git/
-fi
 npm install
+hexo clean
